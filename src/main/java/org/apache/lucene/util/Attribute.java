1fcef03947e4bfcc5ea6b3f334d8ac5acb760b03
Ada.xu <ada.xu@dbappsecurity.com.cn>
2018-05-23 09:32:23 +0800

    ADD 去掉ES和lucene的源码。

--- src/main/java/org/apache/lucene/util/Attribute.java
+++
@@ -1,7 +0,0 @@
-package org.apache.lucene.util;
-
-/**
- * Created by xusiao on 2018/5/7.
- */
-public interface Attribute {
-}
---
+++ src/test/java/org/parc/sqlrestes/TestES.java
@@ -0,0 +1,7 @@
+package org.parc.sqlrestes;
+
+/**
+ * Created by xusiao on 2018/5/17.
+ */
+public class TestES {
+}
